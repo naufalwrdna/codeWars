842ef9f9bbce8de5435115a9b5de400a3a197e46
Naufal Wardana <119428452+naufalwrdna@users.noreply.github.com>
2024-09-12 16:20:47 +0700

    Delete Test-3.js

--- Test-3.js
+++
@@ -1,9 +0,0 @@
-function findNeedle(haystack) {
-    const index = haystack.indexOf('needle');
-
-    return `found the needle at position ${index}`
-  }
-
-  const array = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
-
-  console.log(findNeedle(array))
